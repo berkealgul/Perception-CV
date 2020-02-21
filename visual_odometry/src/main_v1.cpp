0e34bab8bd3e683e3af15c94cf3a559ec872e6c6
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2020-02-21 19:26:20 +0300

    Add files via upload

--- visual_odometry/src/main_v1.cpp
+++ visual_odometry/src/main_v1.cpp
@@ -1,19 +1,31 @@
 #include <iostream>
 #include <math.h>
-#include <ros/ros.h>
-#include <geometry_msgs/Pose.h>
-#include <sensor_msgs/Imu.h>
-#include <opencv2/opencv.hpp>
 #include <vector>
+#include <opencv2/opencv.hpp>
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <geometry_msgs/Pose.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Quaternion.h>
+#include <nav_msgs/Path.h>
 
 using namespace cv;
+using namespace std;
 
-#define MIN_FEAS 500
-#define FastConst 10
+#define MIN_FEAS 800
+#define FastConst 20
+
+
+// TODO : Consider speed's position in code
+float speed = 0; //Speed of the camera
+float sx = 0;
+float sy = 0;
+float sz = 0;
 
 
 class Camera
 {
+ public:
 	//TermiCriteria termCrit TermCriteria::TermCriteria(TermCriteria::COUNT+TermCriteria::EPS, 30, 0.01);
 	Point2d pp;
 	VideoCapture cap;
@@ -28,15 +40,13 @@ class Camera
 
 	    cap.read(rawFrame);
 	    cvtColor(rawFrame, grayFrame, COLOR_BGR2GRAY);
-	    undistort(finalFrame, grayFrame, mtx, dist);
+	    undistort(grayFrame, finalFrame, mtx, dist);
 
 	    return finalFrame;
 	}
 
-	void processFrame(Mat frame, Mat prevImg, Mat R, Mat T, std::vector<Point2f> &prevFeatures)
+	void processFrame(Mat currImg, Mat prevImg, Mat R, Mat T)
 	{
-		Mat currImg;
-		//alýnan resmi siyah-beyaz formata dönüþtür
 		std::vector<Point2f> currFeatures;
 		std::vector<uchar>status;
 
@@ -89,13 +99,22 @@ class Camera
 		}
 	}
 
+	void drawFeatures(Mat src)
+	{
+		for (int i = 0; i < prevFeatures.size(); ++i)
+        {	
+        	Point2f pf = prevFeatures[i];
+        	Point p(int(pf.x), int(pf.y));
+        	circle(src, p, 1, Scalar::all(-1));
+        }	
+	}
 
-public:
+ //public:
 	Mat T, R;
 
 	Camera(Mat _mtx, Mat _dist)
 	{
-	    cap = VideoCapture(0);
+	    cap.open(1);
 	    mtx = _mtx;
 	    dist = _dist;
 
@@ -112,28 +131,31 @@ public:
 	    focal = fx / w;
 	}
 
-	Mat update(float speed)
-	{
+	void update(float speed)
+	{	
+
         Mat frame = getFrame();
+
+        drawFeatures(frame);
+		imshow("frame", frame);
 
 	    if(speed <= 0.1)
 	    {
-	        std::cout << "Low Speed\n";
-	        return frame;
+	        std::cout << "Low Speed: " << speed << "\n";
+	        return;
 	    }
 
 	    Mat dR, dT;
 
 	    if (first == true)
-	    {
+	    {  	
 	        detectFeatures(prevImg, prevFeatures);
-		    processFrame(frame, prevImg, dR, dT, prevFeatures);
+		    processFrame(frame, prevImg, dR, dT);
 	        first = false;
-
 	    }
         else
         {
-            processFrame(frame, prevImg, dR, dT, prevFeatures);
+            processFrame(frame, prevImg, dR, dT);
         }
 
 	    if ((T.at<double>(2) > T.at<double>(1)) && (T.at<double>(2) > T.at<double>(0)))
@@ -143,11 +165,10 @@ public:
 	    }
 	    else
 	        std::cout << "Invalid movement\n";
+
 	}
+
 };
-
-
-float speed = 0; //Speed of the camera
 
 void imuCallback(const sensor_msgs::Imu::ConstPtr& imu)
 {
@@ -155,11 +176,43 @@ void imuCallback(const sensor_msgs::Imu::ConstPtr& imu)
     float ay = imu->linear_acceleration.y;
     float az = imu->linear_acceleration.z;
 
-    speed += sqrt(ax*ax+ay*ay+az*az);
-    std::cout << speed << "\n";
+    sx += ax;
+    sy += ay;
+    sz += az;
+
+    speed += sqrt(sx*sx+sy*sy+sz*sz);
+    //std::cout << speed << "\n";
 }
 
-using namespace std;
+void getQuaternion(Mat R, geometry_msgs::Quaternion Q)
+{
+    double trace = R.at<double>(0,0) + R.at<double>(1,1) + R.at<double>(2,2);
+ 
+    if (trace > 0.0) 
+    {
+        double s = sqrt(trace + 1.0);
+        Q.w = (s * 0.5);
+        s = 0.5 / s;
+        Q.x = ((R.at<double>(2,1) - R.at<double>(1,2)) * s);
+        Q.y = ((R.at<double>(0,2) - R.at<double>(2,0)) * s);
+        Q.z = ((R.at<double>(1,0) - R.at<double>(0,1)) * s);
+    } 
+    
+    else 
+    {
+        int i = R.at<double>(0,0) < R.at<double>(1,1) ? (R.at<double>(1,1) < R.at<double>(2,2) ? 2 : 1) : (R.at<double>(0,0) < R.at<double>(2,2) ? 2 : 0); 
+        int j = (i + 1) % 3;  
+        int k = (i + 2) % 3;
+
+        double s = sqrt(R.at<double>(i, i) - R.at<double>(j,j) - R.at<double>(k,k) + 1.0);
+        Q.x = s * 0.5;
+        s = 0.5 / s;
+
+        Q.w = (R.at<double>(k,j) - R.at<double>(j,k)) * s;
+        Q.y = (R.at<double>(j,i) + R.at<double>(i,j)) * s;
+        Q.z = (R.at<double>(k,i) + R.at<double>(i,k)) * s;
+    }
+}
 
 int main(int argc, char **argv)
 {
@@ -174,20 +227,71 @@ int main(int argc, char **argv)
            0.0, 0.0, 1.0;
 
     ros::NodeHandle nh;
-    ros::Publisher pose_pub = nh.advertise<geometry_msgs::Pose>("/visual_odom_pose", 10);
-    ros::Subscriber imu_sub = nh.subscribe("/imu/data", 10, imuCallback);
+	ros::Publisher path_pub = nh.advertise<nav_msgs::Path>("/visual_odom_path", 10);
+	ros::Subscriber imu_sub = nh.subscribe("/imu/data", 10, imuCallback);
+	ros::Time time;	
+	nav_msgs::Path path;
 
     Camera cam(mtx, dist);
 
     while (ros::ok())
     {
-        Mat frame = cam.update(speed);
-        imshow("frame", frame);
-        ros::spinOnce();
+    	ros::spinOnce();
+    	/*
+    	Mat frame, gray;
+    	cap.read(frame);
+    	std::vector<KeyPoint> keyPoints;
+    	cvtColor(frame, gray, COLOR_BGR2GRAY);
+		FAST(gray, keyPoints, FastConst, true);
+		drawKeypoints(gray, keyPoints, frame);
+    	imshow("frane", frame);
+    	int key = waitKey(10);
+    	continue;
+    	*/
 
-        //TODO:Add pose publisher
+    	//TODO: Draw circle to frame in order to indicate detected features on frame 
+    	//Mat frame;
+        cam.update(speed);
+        int key = waitKey(30);
+        continue;
+        //Draw features
+
+        //Construct and publish calculated data
+
+        time = ros::Time::now();
+
+        geometry_msgs::Pose pose;
+        geometry_msgs::PoseStamped poseStamped;
+        geometry_msgs::Quaternion q;
+        geometry_msgs::Point p;
+
+	    Mat T = cam.T;
+	    Mat R = cam.R;
+	   	
+	   	// define required headers
+	    path.header.stamp = time;
+	 	path.header.frame_id = "vodom";
+	    poseStamped.header.stamp = time;
+	 	poseStamped.header.frame_id = "vodom";
+	 	
+	 	// Fill pose
+	 	p.x = T.at<double>(0,0);
+	 	p.y = T.at<double>(1,0);
+	 	p.z = T.at<double>(2,0);
+	 	getQuaternion(R, q);
+	 	pose.position = p;
+	 	pose.orientation = q;
+
+	 	// fill pose for poseStamped
+	 	poseStamped.pose = pose;
+
+	 	// update path
+	 	path.poses.push_back(poseStamped);
+
+        path_pub.publish(path);
+
+        //int key = waitKey(10);     
     }
 
 	return 0;
-
 }
