f15652f8baa6348a6640d974121bbe83a7c9c516
Berke Algül <47500887+berkealgul@users.noreply.github.com>
2020-09-09 02:07:50 +0300

    initial

--- lidar-camera-fusion/lidar_camera_fusion.cpp
+++ lidar-camera-fusion/lidar_camera_fusion.cpp
@@ -11,122 +11,87 @@
 #include <pcl/visualization/pcl_visualizer.h>
 #include <pcl/io/pcd_io.h>
 #include <pcl/filters/voxel_grid.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/common/transforms.h>
 
 using namespace Eigen;
 using namespace cv;
 
-ros::Publisher pub;
-
-void cloud_cb(const sensor_msgs::PointCloudConstPtr &cloud_msg)
-{
-	sensor_msgs::PointCloud2 cloud2_msg;	
-	sensor_msgs::convertPointCloudToPointCloud2(*cloud_msg, cloud2_msg);
-
-	// Container for original & filtered data
-	pcl::PCLPointCloud2* cloud = new pcl::PCLPointCloud2; 
-	pcl::PCLPointCloud2ConstPtr cloudPtr(cloud);
-	pcl::PCLPointCloud2 cloud_filtered;
-
-	// Convert to PCL data type
-	pcl_conversions::toPCL(cloud2_msg, *cloud);
-  	
-
-	// Perform the actual filtering
-	pcl::VoxelGrid<pcl::PCLPointCloud2> sor;
-	sor.setInputCloud(cloudPtr);
-	sor.setLeafSize(0.1, 0.1, 0.1);
-	sor.filter(cloud_filtered);
-
-	// Convert to ROS data type
-	sensor_msgs::PointCloud2 output;
-	pcl_conversions::fromPCL(*cloud, output);
-
-	pcl::PointCloud<pcl::PointXYZ> cloud_xyz;
-	pcl::fromPCLPointCloud2(cloud_filtered, cloud_xyz);
-
-	// Publish the data
-	pub.publish(output);
-}
-
-float interpolate(float x0, float x1, float y0, float y1, float x)
-{
-	float y;
-	float w = (x-x0)/(x1-x0);
-	y = y0*(1-w) + y1*w;
-	return y;
-}
-
 
 int main(int argc, char** argv)
 {
-	ros::init (argc, argv, "camera_lidar_fusion");
-	
-	ros::NodeHandle nh;
-	ros::Subscriber sub = nh.subscribe("/pointcloud", 1000, cloud_cb);
-	pub = nh.advertise<sensor_msgs::PointCloud2> ("/voxelcloud", 1);
+	ros::init(argc, argv, "camera_lidar_fusion");
 
-	Matrix4f ext(4,4);
-	ext << 1,0,0,0,
-		   0,1,0,0,
-		   0,0,1,0,
-		   0,0,0,1;
+	//KITTI K00 canfig 
 
-	MatrixXf ins(3,4);
-	ins << 517.3, 0.0, 320, 0.0,
-		   0.0, 516.5, 240, 0.0,
-		   0.0, 0.0, 0.0, 1.0;
+	MatrixXf velo_to_cam(3,4);
+	velo_to_cam << 7.533745e-03, -9.999714e-01, -6.166020e-04, -4.069766e-03, 
+				   1.480249e-02, 7.280733e-04, -9.998902e-01, -7.631618e-02,
+				   9.998621e-01, 7.523790e-03, 1.480755e-02, -2.717806e-01;
 
-	MatrixXf pro = ins * ext;
+	MatrixXf cam_to_px(3,3);
+	cam_to_px << 7.215377e+02, 0.000000e+00, 6.095593e+02, 
+		 		 0.000000e+00, 7.215377e+02, 1.728540e+02, 
+		 		 0.000000e+00, 0.000000e+00, 1.000000e+00;
 
-	Mat img = imread("/home/basestation/test_ws/src/test/src/image.png",IMREAD_COLOR);
+	// final projection matrix
+	MatrixXf P = cam_to_px * velo_to_cam;
+
+	Mat img = imread("/home/basestation/KITTI/image.png",IMREAD_COLOR);
 
 	pcl::PointCloud<pcl::PointXYZ>::Ptr cloud(new pcl::PointCloud<pcl::PointXYZ>);
 
-	if (pcl::io::loadPCDFile<pcl::PointXYZ>("/home/basestation/test_ws/src/test/src/cloud.pcd", *cloud) == -1) //* load the file
+	if (pcl::io::loadPCDFile<pcl::PointXYZ>("/home/basestation/KITTI/cloud.pcd", *cloud) == -1) //* load the file
     {
     	PCL_ERROR ("Couldn't read pcd \n");
     	return (-1);
     }
 
     MatrixXf cloud_mat = cloud->getMatrixXfMap(4, 4, 0);
-	MatrixXf img_mat = pro * cloud_mat;
+
+    // final projected pixel coordinates
+	MatrixXf img_mat = P * cloud_mat;
 
 	int w = img.cols;
 	int h = img.rows;
 
-	// max rgb = 7, 237, 245
-	float maxR = 7;
-	float maxG = 237;
-	float maxB = 245;
-	// min rgb = 242, 30, 2
-	float minR = 242;
-	float minG = 30;
-	float minB = 2;
+	//Depth map for depth values
+	ArrayXXf depth_map = ArrayXXf::Zero(h, w);
 
 	for(int i = 0, nCols = img_mat.cols(); i < nCols; i++)
-	{
-		int u = (int)img_mat(0,i);
-		int v = (int)img_mat(1,i);
+	{	
+		float z = img_mat(2,i);
+		int u = img_mat(0,i)/z;
+		int v = img_mat(1,i)/z;
 
-		// if point projected outside of the image
+		// if point projected outside of the image or z is non positive
 		// ignore it
-		if(u < 0 || u >= w || v < 0 || v >= h)
+		if(u < 0 || u >= w || v < 0 || v >= h || z <= 0)
 		{
 			continue;
 		}
 
-		float d = cloud_mat(2, i);
+		depth_map(v, u) = z;
 
-		float r = interpolate(1, 4, minR, maxR, d);
-		float g = interpolate(1, 4, minG, maxG, d);
-		float b = interpolate(1, 4, minB, maxB, d);
+		// rendering purposes not essensial
+		float b = z / 20  * 120;
+		float g = z / 10  * 120;
+		float r = z / 5  * 120;
 
-		circle(img, Point(u,v), 2, Scalar(b,g,r),1);
+		circle(img, Point(u,v), 2, Scalar(b,g,r) ,1);
+
 		//imshow("g", img);	
 		//waitKey(100);
-		std::cout << d << "\n";
+		
 	}
 
 	imwrite("result.png", img);
+	
+	/*
+	// demo purposes
+	pcl::visualization::PCLVisualizer viewer("lol");
+	viewer.addPointCloud(cloud,"velo");
+  	viewer.spin();
+	*/
 	return 0;
 }
